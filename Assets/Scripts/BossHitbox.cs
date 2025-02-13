c6afe2eca9f11c82ea054ccbe58394e4254a4c07
Ryan Dostalek <rdostale@calpoly.edu>
2025-02-12 19:26:37 -0800

    Added: boss flashing on damage

--- Assets/Scripts/BossHitbox.cs
+++ Assets/Scripts/BossHitbox.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEditor.UI;
 using UnityEngine;
 
@@ -6,11 +7,45 @@ public class BossHitbox : MonoBehaviour
 {
     public float health = 100f;
     public GameObject parent;
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] private float flashDuration = 0.1f;
+    private bool isFlashing;
 
-    public void takeDamage(float damage){
+    void Start()
+    {
+        spriteRenderer = parent.GetComponent<SpriteRenderer>();
+    }
+
+    public void takeDamage(float damage)
+    {
         health -= damage;
-        if (health <= 0){
+        
+        if (!isFlashing)
+        {
+            StartCoroutine(onDamage());
+        }
+        
+        if (health <= 0)
+        {
             Destroy(parent);
         }
+    }
+
+    private IEnumerator onDamage()
+    {
+        isFlashing = true;
+        Color color = spriteRenderer.color;
+
+        spriteRenderer.color = new Color(
+            color.r * 1.5f,
+            color.g * 1.5f,
+            color.b * 1.5f,
+            color.a
+        );
+
+        yield return new WaitForSeconds(flashDuration);
+
+        spriteRenderer.color = color;
+        isFlashing = false;
     }
 }
